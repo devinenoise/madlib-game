83ccf2235ec21af23fd85da6d6b648f3508a6f25
Kyle Devine <devinenoise@gmail.com>
2020-01-09 20:47:44 -0800

    created html and javascript

---
+++ app.js
@@ -0,0 +1,57 @@
+const adj1Input = document.getElementById('adj1Input');
+const adj1Output = adj1Input.value;
+adj1.textContent = adj1Output;
+
+const adj2Input = document.getElementById('adj2Input');
+const adj2Output = adj2Input.value;
+adj2.textContent = adj2Output;
+
+const birdInput = document.getElementById('birdInput');
+const birdOutput = birdInput.value;
+bird.textContent = birdOutput;
+
+const roomInput = document.getElementById('roomInput');
+const roomOutput = roomInput.value;
+room.textContent = roomOutput;
+
+const verb1Input = document.getElementById('verb1Input');
+const verb1Output = verb1Input.value;
+verb1.textContent = verb1Output;
+
+const verb2Input = document.getElementById('verb2Input');
+const verb2Output = verb2Input.value;
+verb2.textContent = verb2Output;
+
+const relativeInput = document.getElementById('relativeInput');
+const relativeOutput = relativeInput.value;
+relative.textContent = relativeOutput;
+
+const nounInput = document.getElementById('nounInput');
+const nounOutput = nounInput.value;
+noun.textContent = nounOutput;
+
+const liquidInput = document.getElementById('liquidInput');
+const liquidOutput = liquidInput.value;
+liquid.textContent = liquidOutput;
+
+const verbing1Input = document.getElementById('verbing1Input');
+const verbing1Output = verbign1Input.value;
+verbing1.textContent = verbing1Output;
+
+const bodyInput = document.getElementById('bodyInput');
+const bodyOutput = bodyInput.value;
+body.textContent = bodyOutput;
+
+const nounpluralInput = document.getElementById('nounpluralInput');
+const nounpluralOutput = nounpluralInput.value;
+nounplural.textContent = nounpluralOutput;
+
+const verbing2Input = document.getElementById('verbing2Input');
+const verbing2Output = verbing2Input.value;
+verbing2.textContent = verbing2Output;
+
+const noun2Input = document.getElementById('noun2Input');
+const noun2Output = noun2Input.value;
+noun2.textContent = noun2Output;
+
+document.getElementById('storyResults').style.visibility = 'visible';
