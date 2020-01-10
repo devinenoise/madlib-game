ff49748b770afe0c13f8d406f10a3e647c7a64b7
Kyle Devine <devinenoise@gmail.com>
2020-01-10 11:12:19 -0800

    it works now

--- app.js
+++ app.js
@@ -1,57 +1,68 @@
-const adj1Input = document.getElementById('adj1Input');
-const adj1Output = adj1Input.value;
-adj1.textContent = adj1Output;
+document.getElementById("button").addEventListener("click", fillInBlanks);
 
-const adj2Input = document.getElementById('adj2Input');
-const adj2Output = adj2Input.value;
-adj2.textContent = adj2Output;
 
-const birdInput = document.getElementById('birdInput');
-const birdOutput = birdInput.value;
-bird.textContent = birdOutput;
+function fillInBlanks() {
 
-const roomInput = document.getElementById('roomInput');
-const roomOutput = roomInput.value;
-room.textContent = roomOutput;
+  const adj1Input = document.getElementById('adj1Input');
+  const adj1Output = adj1Input.value;
+  adj1.textContent = adj1Output;
 
-const verb1Input = document.getElementById('verb1Input');
-const verb1Output = verb1Input.value;
-verb1.textContent = verb1Output;
+  const adj2Input = document.getElementById('adj2Input');
+  const adj2Output = adj2Input.value;
+  adj2.textContent = adj2Output;
 
-const verb2Input = document.getElementById('verb2Input');
-const verb2Output = verb2Input.value;
-verb2.textContent = verb2Output;
+  const birdInput = document.getElementById('birdInput');
+  const birdOutput = birdInput.value;
+  bird.textContent = birdOutput;
 
-const relativeInput = document.getElementById('relativeInput');
-const relativeOutput = relativeInput.value;
-relative.textContent = relativeOutput;
+  const roomInput = document.getElementById('roomInput');
+  const roomOutput = roomInput.value;
+  room.textContent = roomOutput;
 
-const nounInput = document.getElementById('nounInput');
-const nounOutput = nounInput.value;
-noun.textContent = nounOutput;
+  const verb1Input = document.getElementById('verb1Input');
+  const verb1Output = verb1Input.value;
+  verb1.textContent = verb1Output;
 
-const liquidInput = document.getElementById('liquidInput');
-const liquidOutput = liquidInput.value;
-liquid.textContent = liquidOutput;
+  const verb2Input = document.getElementById('verb2Input');
+  const verb2Output = verb2Input.value;
+  verb2.textContent = verb2Output;
 
-const verbing1Input = document.getElementById('verbing1Input');
-const verbing1Output = verbign1Input.value;
-verbing1.textContent = verbing1Output;
+  const relativeInput = document.getElementById('relativeInput');
+  const relativeOutput = relativeInput.value;
+  relative.textContent = relativeOutput;
 
-const bodyInput = document.getElementById('bodyInput');
-const bodyOutput = bodyInput.value;
-body.textContent = bodyOutput;
+  const nounInput = document.getElementById('nounInput');
+  const nounOutput = nounInput.value;
+  noun.textContent = nounOutput;
 
-const nounpluralInput = document.getElementById('nounpluralInput');
-const nounpluralOutput = nounpluralInput.value;
-nounplural.textContent = nounpluralOutput;
+  const liquidInput = document.getElementById('liquidInput');
+  const liquidOutput = liquidInput.value;
+  liquid.textContent = liquidOutput;
 
-const verbing2Input = document.getElementById('verbing2Input');
-const verbing2Output = verbing2Input.value;
-verbing2.textContent = verbing2Output;
+  const verbing1Input = document.getElementById('verbing1Input');
+  const verbing1Output = verbing1Input.value;
+  verbing1.textContent = verbing1Output;
 
-const noun2Input = document.getElementById('noun2Input');
-const noun2Output = noun2Input.value;
-noun2.textContent = noun2Output;
+  const bodyInput = document.getElementById('bodyInput');
+  const bodyOutput = bodyInput.value;
+  body.textContent = bodyOutput;
 
-document.getElementById('storyResults').style.visibility = 'visible';
+  const nounpluralInput = document.getElementById('nounpluralInput');
+  const nounpluralOutput = nounpluralInput.value;
+  nounplural.textContent = nounpluralOutput;
+
+  const verbing2Input = document.getElementById('verbing2Input');
+  const verbing2Output = verbing2Input.value;
+  verbing2.textContent = verbing2Output;
+
+  const noun2Input = document.getElementById('noun2Input');
+  const noun2Output = noun2Input.value;
+  noun2.textContent = noun2Output;
+
+  document.getElementById("storyResults").style.visibility = "visible";
+
+};
+
+
+
+
